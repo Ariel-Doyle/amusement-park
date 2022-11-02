0abf685607628bf416a58305c7af696674b392ca
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-11-01 19:43:41 -0700

    add error message function to js, css, and HTML.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-function hideResults() {
+function hideResultsAndError() {
+  document.getElementById("error-message").setAttribute("class" , "hidden");
   document.getElementById("swings").setAttribute("class", "hidden");
   document.getElementById("coaster").setAttribute("class", "hidden");
   document.getElementById("tower").setAttribute("class", "hidden");
@@ -8,20 +9,25 @@ function hideResults() {
 window.onload = function() {
   document.querySelector("form").onsubmit = function(event) {
     event.preventDefault();
-    hideResults();
+    hideResultsAndError();
     const age = parseInt(document.querySelector("input#age").value);
     const height = parseInt(document.querySelector("input#height").value);
-  };
-};
-
-document.querySelector("form").onsubmit = function(event) {
-  event.preventDefault();
-  hideResults();
-  const age = parseInt(document.querySelector("input#age").value);
-  const height = parseInt(document.querySelector("input#height").value);
-  if (age >= 12 && height >= 60) {
-    document.getElementById("swings").removeAttribute("class");
-    document.getElementById("coaster").removeAttribute("class");
-    document.getElementById("tower").removeAttribute("class");
-  } 
+    
+    if (age && height) {
+      if (age >= 12 && height >= 60) {
+        document.getElementById("swings").removeAttribute("class");
+        document.getElementById("coaster").removeAttribute("class");
+        document.getElementById("tower").removeAttribute("class");
+      } else if (age >= 12 || height >= 48) {
+        document.getElementById("swings").removeAttribute("class");
+        document.getElementById("coaster").removeAttribute("class");
+      } else if (age >= 6) {
+        document.getElementById("swings").removeAttribute("class");
+      } else {
+        document.getElementById("sorry").removeAttribute("class");
+      } 
+    } else { 
+      document.getElementById("error-message").removeAttribute("class");
+    }
+  }
 };
